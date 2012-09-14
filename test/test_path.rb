98d2a1375723d1b6cadd29d08a2271f71d036273
Lee Dohm <lee@liftedstudios.com>
2012-09-14 01:17:42 -0700

    Change from regexes to exact string matching.

--- test/test_path.rb
+++ test/test_path.rb
@@ -6,26 +6,26 @@ require File.expand_path(File.join(File.dirname(__FILE__), "helper"))
 
 context "Path" do
   test "filename to name" do
-    assert_match %r{^Filename$}, Gollum::Path.to_name("Filename.md")
+    assert_equal 'Filename', Gollum::Path.to_name("Filename.md")
   end
 
   test "filename with spaces to name" do
-    assert_match %r{^Filename With Spaces$}, Gollum::Path.to_name("Filename_With_Spaces.md")
+    assert_equal 'Filename With Spaces', Gollum::Path.to_name("Filename_With_Spaces.md")
   end
 
   test "path to name" do
-    assert_match %r{^Filename$}, Gollum::Path.to_name("path/to/file/Filename.md")
+    assert_equal 'Filename', Gollum::Path.to_name("path/to/file/Filename.md")
   end
 
   test "path with spaces to name" do
-    assert_match %r{^Filename With Spaces$}, Gollum::Path.to_name("path/to/file/Filename_With_Spaces.md")
+    assert_equal 'Filename With Spaces', Gollum::Path.to_name("path/to/file/Filename_With_Spaces.md")
   end
 
   test "filename with url encoded spaces to name" do
-    assert_match %r{^Filename With Spaces$}, Gollum::Path.to_name("Filename+With+Spaces.md")
+    assert_equal 'Filename With Spaces', Gollum::Path.to_name("Filename+With+Spaces.md")
   end
 
   test "filename with url encoded underscores to name" do
-    assert_match %r{^Filename With Special Characters$}, Gollum::Path.to_name("Filename%5FWith%5FSpecial%5FCharacters.md")
+    assert_equal 'Filename With Special Characters', Gollum::Path.to_name("Filename%5FWith%5FSpecial%5FCharacters.md")
   end
 end
